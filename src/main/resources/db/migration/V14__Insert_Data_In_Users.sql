60ac1b2491ccf61fc559793d6e909ac7adbe5dc0
Flávio Alexandre <flavioalexandre115@gmail.com>
2025-08-21 15:04:02 -0300

    feat: criando as migrações com flyway para criar e alimentar as tabelas de usuarios e permissoes, garantindo prosseguimento do sistema de autenticação no backend.

---
+++ src/main/resources/db/migration/V14__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+('flavio.severiano', 'Flávio Alexandre', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
+('admin', 'Administrador', '1e3cdeeaa)aeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1');
